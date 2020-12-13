6999656d2a0c694ed5f44241b17e3a9d65ef1bbe
Roman <romankp@gmail.com>
2020-12-12 20:18:37 -0500

    add constant import

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { key, stationId } from '../constants.js';
 
 const App = () => (
   <React.Fragment>
     <h1>Tides</h1>
     <p>This is where tide heights will eventually show up.</p>
+    <p>{key}</p>
+    <p>{stationId}</p>
   </React.Fragment>
 );
 

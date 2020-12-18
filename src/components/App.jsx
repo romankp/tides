4ae2d086951f09267dee141d0dfe35331da35123
Roman <romankp@gmail.com>
2020-12-17 22:24:07 -0500

    init fetch URL

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,28 @@
-import React from "react";
-import { key, stationId } from "../constants.js";
+import React from 'react';
+import { token, baseUrl, stationId } from '../constants.js';
+
+const startDate = 20201216;
+const endDate = 20201217;
+const completeUrl = `${baseUrl}?station=${stationId}&datum=STND&time_zone=lst&begin_date=${startDate}&end_date=${endDate}&units=english&format=json&product=predictions&interval=hilo`;
+
+const fetchStationData = async () => {
+  const response = await fetch(completeUrl).catch((e) => {
+    console.error(`Fetch request failed: ${e}`);
+  });
+  const data = await response.json();
+  return data;
+};
+
+fetchStationData().then((data) => {
+  console.log(JSON.stringify(data));
+});
 
 const App = () => (
-  <React.Fragment>
+  <div>
     <h1>Tides</h1>
     <p>This is where tide heights will eventually show up.</p>
-    <p>{key}</p>
-    <p>{stationId}</p>
-  </React.Fragment>
+    <p>{completeUrl}</p>
+  </div>
 );
 
 export default App;

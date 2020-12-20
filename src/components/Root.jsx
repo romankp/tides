a30fe419094120d0c149a280c1abe91a374d959d
Roman <romankp@gmail.com>
2020-12-20 17:56:52 -0500

    fade-in & time parsing

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,13 +1,13 @@
 import React, { Component, Fragment } from 'react';
 import { baseUrl, stationId } from '../constants.js';
 
-const startDate = 20201216;
-const endDate = 20201217;
+// const startDate = 20201216;
+// const endDate = 20201217;
 // const urlFull = `${baseUrl}?station=${stationId}&datum=STND&time_zone=lst&begin_date=${startDate}&end_date=${endDate}&units=english&format=json&product=predictions&interval=hilo`;
 const urlToday = `${baseUrl}?station=${stationId}&datum=STND&time_zone=lst&date=today&units=english&format=json&product=predictions&interval=hilo`;
 
-const fetchTideData = async () => {
-  const response = await fetch(urlToday).catch((e) => {
+const fetchTideData = async (url) => {
+  const response = await fetch(url).catch((e) => {
     console.error(`Fetch request failed: ${e}`);
   });
   const data = await response.json();
@@ -24,31 +24,41 @@ class Root extends Component {
         day: 'numeric',
         timeZone: 'est'
       }),
+      loaded: false,
       predictionsArray: []
     };
   }
 
   componentDidMount() {
-    fetchTideData().then((data) => {
+    fetchTideData(urlToday).then((data) => {
       const { predictions } = data;
       console.log(JSON.stringify(predictions));
       this.setState({
-        predictionsArray: predictions
+        predictionsArray: predictions,
+        loaded: true
       });
     });
   }
 
   render() {
+    let { loaded, currentDate, predictionsArray } = this.state;
     return (
-      <Fragment>
+      <div className={`main${loaded && ' show'}`}>
         <h1>Tides</h1>
-        <h2>Today {this.state.currentDate}</h2>
-        {this.state.predictionsArray.map((prediction) => (
-          <p key={prediction.t}>
-            {prediction.type} {prediction.t}
-          </p>
-        ))}
-      </Fragment>
+        <h2>Today / {currentDate}</h2>
+        {predictionsArray.map((prediction) => {
+          const timeString = new Date(prediction.t);
+          const localizedTime = timeString.toLocaleTimeString('en-US', {
+            hour: '2-digit',
+            minute: '2-digit'
+          });
+          return (
+            <p key={prediction.t}>
+              {prediction.type} {localizedTime}
+            </p>
+          );
+        })}
+      </div>
     );
   }
 }

a4c215b59750fe218c04743bce949ec1c6e6a53c
Roman <romankp@gmail.com>
2020-12-19 21:56:28 -0500

    initial style

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import { baseUrl, stationId } from '../constants.js';
 
 const startDate = 20201216;
 const endDate = 20201217;
-const completeUrl = `${baseUrl}?station=${stationId}&datum=STND&time_zone=lst&begin_date=${startDate}&end_date=${endDate}&units=english&format=json&product=predictions&interval=hilo`;
+// const urlFull = `${baseUrl}?station=${stationId}&datum=STND&time_zone=lst&begin_date=${startDate}&end_date=${endDate}&units=english&format=json&product=predictions&interval=hilo`;
+const urlToday = `${baseUrl}?station=${stationId}&datum=STND&time_zone=lst&date=today&units=english&format=json&product=predictions&interval=hilo`;
 
 const fetchTideData = async () => {
-  const response = await fetch(completeUrl).catch((e) => {
+  const response = await fetch(urlToday).catch((e) => {
     console.error(`Fetch request failed: ${e}`);
   });
   const data = await response.json();
   return data;
 };
 
-class App extends React.Component {
+class Root extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -33,16 +34,17 @@ class App extends React.Component {
 
   render() {
     return (
-      <React.Fragment>
+      <Fragment>
         <h1>Tides</h1>
         {this.state.predictionsArray.map((prediction) => (
           <p key={prediction.t}>
             {prediction.type} {prediction.t}
           </p>
         ))}
-      </React.Fragment>
+        <p>{urlToday}</p>
+      </Fragment>
     );
   }
 }
 
-export default App;
+export default Root;

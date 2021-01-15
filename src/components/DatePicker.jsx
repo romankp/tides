4abdacd26ea98a4fa59cbc4ac9efd6a26fb734eb
Roman <romankp@gmail.com>
2021-01-14 20:36:51 -0500

    date picker structure

--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 
+// Tide prediction is limited to 10 years from current date
+
 const DatePicker = () => {
   return (
     <form className="datePicker">
-      <label for="month">Choose month:</label>
+      <label for="month">Choose date:</label>
       <select name="month" id="month" className="dropdownItem">
-          <option value="01">January</option>
-          <option value="02">February</option>
+        <option value="01">January</option>
+        <option value="02">February</option>
+      </select>
+      <select name="day" id="day" className="dropdownItem">
+        <option value="01">01</option>
+        <option value="02">02</option>
+      </select>
+      <select name="year" id="year" className="dropdownItem">
+        <option value="01">2021</option>
+        <option value="02">2022</option>
       </select>
     </form>
   );

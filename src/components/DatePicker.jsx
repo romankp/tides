4fbbaaaeeecf3c8b10602e7e6200cf2c387bbb4c
Roman <romankp@gmail.com>
2021-03-31 22:05:01 -0400

    calendar state

--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
@@ -7,10 +7,12 @@ import { months } from '../utils/constants.js';
 // Tide prediction is limited to 10 years from current date
 
 const DatePicker = ({ date }) => {
+  const [value, onChange] = useState(date);
   console.log(date);
+  console.log(value);
   return (
     <div className="datePicker">
-      <Calendar calendarType="US" />
+      <Calendar calendarType="US" onChange={onChange} value={value} />
     </div>
   );
 };

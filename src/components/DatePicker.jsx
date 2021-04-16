c2cacb5cbe2d1d5d6f194a54490f9b1fc8acaf9c
Roman <romankp@gmail.com>
2021-04-15 22:21:17 -0400

    update component

--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Calendar from 'react-calendar';
 import { localizeTime } from '../utils/componentUtils.js';
@@ -10,6 +10,27 @@ const hilo = {
   L: 'low',
 };
 
+const FuturePredictions = (
+  futureLoaded,
+  formattedDate,
+  predictionsFuture,
+  hilo
+) => (
+  <div className={!futureLoaded ? 'hide' : null}>
+    <h2>{formattedDate}</h2>
+    <ol>
+      {predictionsFuture.map(({ type, t }) => {
+        return (
+          <li key={`${type}${t}`}>
+            {hilo[type]} {localizeTime(t)}
+          </li>
+        );
+      })}
+    </ol>
+    <span className={'back'}>&#8617;</span>
+  </div>
+);
+
 const DatePicker = ({
   date,
   onDateChange,
@@ -19,30 +40,21 @@ const DatePicker = ({
 }) => {
   return (
     <div className="datePicker">
-      <Calendar
-        calendarType="US"
-        onChange={updatedDate => {
-          // This is a temporary solution to accommodate DST in my area.
-          // We don't really need to know the time of day right now.
-          // TODO: Dynamic check for offset
-          const adjustedDate = new Date(updatedDate.setHours(2));
-          onDateChange(adjustedDate);
-        }}
-        value={date}
-      />
-      {futureLoaded && (
-        <Fragment>
-          <h2>{formattedDate}</h2>
-          <ol>
-            {predictionsFuture.map(({ type, t }) => {
-              return (
-                <li key={`${type}${t}`}>
-                  {hilo[type]} {localizeTime(t)}
-                </li>
-              );
-            })}
-          </ol>
-        </Fragment>
+      {futureLoaded ? (
+        FuturePredictions(futureLoaded, formattedDate, predictionsFuture, hilo)
+      ) : (
+        <Calendar
+          calendarType="US"
+          onChange={updatedDate => {
+            // This is a temporary solution to accommodate DST in my area.
+            // We don't really need to know the time of day right now.
+            // TODO: Dynamic check for offset
+            const adjustedDate = new Date(updatedDate.setHours(2));
+            onDateChange(adjustedDate);
+          }}
+          value={date}
+          className={futureLoaded ? 'hide' : null}
+        />
       )}
     </div>
   );
